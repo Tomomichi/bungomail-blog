219854ea90c0a4a8f24581cb966d8fffae7b1edb
notsobad <tomomichi.onishi@gmail.com>
2020-06-13 15:32:39 +0900

    Create index page

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,50 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
-import Meta from '../components/meta'
+import Head from 'next/head'
+import Link from 'next/link'
 
-export default function Layout({ preview, children }) {
+export default function Layout({ children }) {
   return (
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
+    <div>
+      <Head>
+        <meta content="width=device-width, initial-scale=1, minimum-scale=1" name="viewport" />
+        <link rel="icon" href="/images/notsobad/favicon.ico" />
+        <title key="title">NOT SO BADなブログ</title>
+        <meta name="description" content="ぼっちスタートアップが日々がんばっています。" />
+        <meta content="NOT SO BADなブログ" property="og:title" key="og:title" />
+        <meta content="website" property="og:type" />
+        <meta content="https://blog.notsobad.jp" property="og:url" key="og:url" />
+        <meta content="https://blog.notsobad.jp/images/notsobad/ogp.png" property="og:image" key="og:image" />
+        <meta content="ぼっちスタートアップが日々がんばっています。" property="og:description" key="og:description" />
+        <meta content="NOT SO BADなブログ" property="og:site_name" />
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta content="NOT SO BADなブログ" name="twitter:title" key="twitter:title" />
+        <meta content="ぼっちスタートアップが日々がんばっています。" name="twitter:description" key="twitter:description" />
+        <meta content="https://blog.notsobad.jp/images/notsobad/ogp.png" name="twitter:image:src" key="twitter:image:src" />
+      </Head>
+
+      <section className="text-gray-700 body-font">
+        <div className="container px-5 md:px-24 p-5 md:pb-12 mx-auto max-w-screen-lg">
+          <h1 className="text-xl font-bold title-font text-gray-900 mb-4 inline-block border-b-4 border-gray-900">
+            <Link href='/'>
+              <a>NOT SO BAD なブログ</a>
+            </Link>
+          </h1>
+          <h2 className="text-xs text-gray-700 tracking-widest font-medium title-font">ぼっちスタートアップが日々がんばっています。</h2>
+        </div>
+      </section>
+
+      { children }
+
+      <footer className="text-gray-200 body-font">
+        <div className="bg-gray-900">
+          <div className="container mx-auto py-6 px-5 md:px-24 flex flex-wrap flex-col sm:flex-row max-w-screen-lg">
+            <p className="text-gray-200 text-sm text-center sm:text-left">
+              <Link href='/'>
+                <a className="text-gray-500 hover:text-gray-200 ml-1">© 2020 NOT SO BADなブログ</a>
+              </Link>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,46 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Link from 'next/link'
+import Layout from '../components/layout'
+import ReactMarkdown from 'react-markdown'
+import { getAllPosts } from '../lib/api'
+import { formatDate } from '../lib/utilities'
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
+        <section className="text-gray-700 body-font overflow-hidden break-all">
+          <div className="container px-5 md:px-24 pb-24 mx-auto max-w-screen-lg">
+            <div className="-my-8">
+              { allPosts.map((item) => (
+                <div className="py-12 flex flex-wrap md:flex-no-wrap" key={ item.slug }>
+                  <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                    <span className="mt-1 text-gray-900 font-bold">{ formatDate(item.date) }</span>
+                  </div>
+                  <div className="md:flex-grow">
+                    <h2 className="text-2xl md:text-3xl font-bold text-gray-900 title-font mb-4">
+                      <Link href="/entry/[...slug]" as={`/entry/${ item.slug }`}>
+                        <a>{ item.title }</a>
+                      </Link>
+                    </h2>
+                    <div className="leading-relaxed">
+                      <ReactMarkdown source={ item.excerpt } escapeHtml={ false } />
+                    </div>
+
+                    <Link href="/entry/[...slug]" as={`/entry/${ item.slug }`}>
+                      <a className="text-blue-700 inline-flex items-center mt-4">記事の続きを読む
+                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              )) }
+            </div>
+          </div>
+        </section>
       </Layout>
     </>
   )
