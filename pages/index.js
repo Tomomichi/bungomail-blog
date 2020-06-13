4f13d2b2e01f5009ec7c4b4839e831c8fbaa7878
notsobad <tomomichi.onishi@gmail.com>
2020-06-13 17:50:20 +0900

    Import past articles

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ allPosts }) {
             <div class="mb-12 flex flex-col items-start" key={ item.slug }>
               <span class="inline-block py-1 px-3 rounded bg-gray-100 text-gray-700 text-sm font-medium tracking-widest">{ formatDate(item.date) }</span>
               <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">{ item.title }</h2>
-              <p class="leading-relaxed mb-8">{ item.excerpt }</p>
+              <p class="leading-relaxed mb-8">{ item.content.slice(0, 200) }...</p>
             </div>
           )) }
         </section>
@@ -28,9 +28,7 @@ export async function getStaticProps() {
     'title',
     'date',
     'slug',
-    'author',
-    'coverImage',
-    'excerpt',
+    'content',
   ])
 
   return {
