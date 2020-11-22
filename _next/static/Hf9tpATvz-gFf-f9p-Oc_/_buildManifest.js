a0dee9050f4980e7df39b497658d6cb5716916d9
Tomomichi <Tomomichi@users.noreply.github.com>
2020-11-22 22:23:27 +0000

    deploy: f7a9500c72b4caa224c1254e209efa833d60f101

--- _next/static/Hf9tpATvz-gFf-f9p-Oc_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/entry/[...slug]":[a],"/index":[a,b],"/page/[page]":[a,b]}}("static\u002Fchunks\u002Fb777ecef7b4e6c75db7379edd1299ebf0a3ac38e.f31ca66a10d9774036d9.js","static\u002Fchunks\u002F763992d9ac0c85266d5f48d86607038dbc26f5b1.24fba51b7b26e51762df.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
