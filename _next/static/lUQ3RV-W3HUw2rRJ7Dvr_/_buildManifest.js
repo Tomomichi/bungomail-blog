0a9dacec0d1a706f0c03a41c11ce94cdbfacdfe2
Tomomichi <Tomomichi@users.noreply.github.com>
2021-02-20 22:27:58 +0000

    deploy: 634042487cf3c553406d477db35f0f2c7bb63d42

--- _next/static/lUQ3RV-W3HUw2rRJ7Dvr_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/entry/[...slug]":[a],"/index":[a,b],"/page/[page]":[a,b]}}("static\u002Fchunks\u002Fa25dbfb5c1a29af790a74cadc689bc176958204f.f31ca66a10d9774036d9.js","static\u002Fchunks\u002F31720fffb6a0d60f1e80ef4b2e6a916db26dd3cf.24fba51b7b26e51762df.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
