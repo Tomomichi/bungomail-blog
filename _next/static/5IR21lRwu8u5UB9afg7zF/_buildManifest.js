a53680271a24ab5e784469e2c4575357335343d8
Tomomichi <Tomomichi@users.noreply.github.com>
2021-05-21 22:31:03 +0000

    deploy: 4104168e47a60bfcff7d850f4c3f997cafbee300

--- _next/static/5IR21lRwu8u5UB9afg7zF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/entry/[...slug]":[a],"/index":[a,b],"/page/[page]":[a,b]}}("static\u002Fchunks\u002Ff84449d33ade0e864ca6d374f0bed7a0a98a96a0.f31ca66a10d9774036d9.js","static\u002Fchunks\u002F1e0cec86d8a06ad09f17e28dd3000cc8ebfa3e53.24fba51b7b26e51762df.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
