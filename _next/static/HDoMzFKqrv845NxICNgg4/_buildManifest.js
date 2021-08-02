b6c5e28e9f3096262fa269ba8ee5f24edae53e1d
Tomomichi <Tomomichi@users.noreply.github.com>
2021-08-02 22:27:15 +0000

    deploy: 166f1b9d45a7c8c73d000ac33b2c6700d8c8c8b5

---
+++ _next/static/HDoMzFKqrv845NxICNgg4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/entry/[...slug]":[a],"/index":[a,b],"/page/[page]":[a,b]}}("static\u002Fchunks\u002F713c828d1227e6014ba936e7d2c0dd3fce7bc461.f31ca66a10d9774036d9.js","static\u002Fchunks\u002F9e4b3323ca87acff85f78764644dbd925386ccd3.24fba51b7b26e51762df.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/VewOyEXGJNs0KGaM32aw4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/entry/[...slug]":[a],"/index":[a,b],"/page/[page]":[a,b]}}("static\u002Fchunks\u002F510aa44f437b0c6dadfa65b67ca0c08c9fa1bf06.f31ca66a10d9774036d9.js","static\u002Fchunks\u002Fd3ae167cb2f80b13df7dd8d0a7bcd016516145f7.24fba51b7b26e51762df.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
